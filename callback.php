5aed20854eb234f48ab935cfea40f29281877d4a
Jim Burger <jim.burger@certegy.com.au>
2016-08-19 10:58:30 +0930

    ...cancel that theory

--- callback.php
+++ callback.php
@@ -10,24 +10,24 @@ function payment_finalisation($order_id) {
     $response = wp_remote_get($config['XPAY_URL'] . $order_id . '.json');
     $body = json_decode( wp_remote_retrieve_body($response) );
 
-    // $processing_status = 
+    $processing_status = 
 
-    // Get the status of the order from XPay and handle accordingly
-    // switch ($processing_status) {
+    Get the status of the order from XPay and handle accordingly
+    switch ($processing_status) {
 
-    //     case 'APPROVED':
-    //         $order->add_order_note( __( 'Payment approved using ' . $config['XPAY_DISPLAYNAME'] . '. Your Order ID is '. $order->id, 'woocommerce' ) );
-    //         $order->payment_complete($response->id);
-    //         woocommerce_empty_cart();
+        case 'APPROVED':
+            $order->add_order_note( __( 'Payment approved using ' . $config['XPAY_DISPLAYNAME'] . '. Your Order ID is '. $order->id, 'woocommerce' ) );
+            $order->payment_complete($response->id);
+            woocommerce_empty_cart();
 
-    //     case 'PENDING':
-    //         $order->add_order_note( __( 'Payment pending using ' . $config['XPAY_DISPLAYNAME'] . '. Your Order ID is '. $order->id, 'woocommerce' ) );
-    //         $order->update_status('on-hold');
+        case 'PENDING':
+            $order->add_order_note( __( 'Payment pending using ' . $config['XPAY_DISPLAYNAME'] . '. Your Order ID is '. $order->id, 'woocommerce' ) );
+            $order->update_status('on-hold');
 
-    //     case 'DECLINED':
-    //         $order->add_order_note( __( 'Payment declined using ' . $config['XPAY_DISPLAYNAME'] . '. Your Order ID is '. $order->id, 'woocommerce' ) );
-    //         $order->update_status('failed');
-    // }
+        case 'DECLINED':
+            $order->add_order_note( __( 'Payment declined using ' . $config['XPAY_DISPLAYNAME'] . '. Your Order ID is '. $order->id, 'woocommerce' ) );
+            $order->update_status('failed');
+    }
 
     return $order_id;
 
