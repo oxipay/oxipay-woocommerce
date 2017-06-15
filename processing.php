63f3db03658e217a107615910b5965b12e47e54b
Andrew Mason <andrew.mason@oxipay.com.au>
2017-06-15 15:33:43 +0930

    Remove Oxipay branding so that we can migrate to new branding without having to update all the time

--- processing.php
+++ processing.php
@@ -1,22 +1,47 @@
 <html>
 <head>
-    <title>Processing Payment</title>
+    <title>Redirecting to Oxipay</title>
     <link rel="stylesheet" href="css/oxipay.css">
     <meta name="viewport" content="width=device-width">
+    <script src="js/lib/spin.min.js"></script>    
 </head>
 <body>
-<div id="top-bar">
-    <img id="logo" src="images/oxipay.svg">
-</div>
-<div class="card">
-    <div class="card-block card-heading"> <h4> Processing</h4> </div>
-    <p>Please wait while we process your request</p>
-    <div id="spinner"></div>
-</div>
 
-<!-- spinner animation -->
-<script src="js/lib/spin.min.js"></script>
-<script src="js/spinner.js"></script>
+    <div id="wrapper">
+        <div class="card">
+            <div class="card-block card-heading"> <h4>Redirecting</h4> </div>
+            <p>Please wait while we redirect you to Oxipay</p>
+            <div id="spinner"></div>
+        </div>
+    </div>
+
+<script >
+        var opts = {
+            lines: 50 // The number of lines to draw
+            , length: -5 // The length of each line
+            , width: 14 // The line thickness
+            , radius: 20 // The radius of the inner circle
+            , scale: 1 // Scales overall size of the spinner
+            , corners: 1 // Corner roundness (0..1)
+            , color: '#e68821' // #rgb or #rrggbb or array of colors
+            , opacity: 0 // Opacity of the lines
+            , rotate: 0 // The rotation offset
+            , direction: 1 // 1: clockwise, -1: counterclockwise
+            , speed: 1 // Rounds per second
+            , trail: 50 // Afterglow percentage
+            , fps: 30 // Frames per second when using setTimeout() as a fallback for CSS
+            , zIndex: 2e9 // The z-index (defaults to 2000000000)
+            , className: 'spinner' // The CSS class to assign to the spinner
+            , top: '50%' // Top position relative to parent
+            , left: '50%' // Left position relative to parent
+            , shadow: false // Whether to render a shadow
+            , hwaccel: true // Whether to use hardware acceleration
+            , position: 'absolute' // Element positioning
+        };
+
+        var target = document.getElementById('spinner');
+        var spinner = new Spinner(opts).spin(target);
+</script>    
 
 <?php
 include_once( 'oxipay-config.php' );
