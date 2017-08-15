02e6c67bc4f460076faf3f9949f50ee5a3070611
tao wang <hellotaotao@gmail.com>
2017-08-16 08:48:59 +0930

    add/replace js/css files

--- js/oxipay_modal.js
+++ js/oxipay_modal.js
@@ -1,25 +1,34 @@
 ///<reference path="../typings/jquery/jquery.d.ts"/>
 function oxipay(q) {
+    var initialised = false;
     var iframeId = 'oxipay-iframe';
     var data;
     var form = null;
-    //var stylesheetUrl = '/Modal/src/css/oxipay.css';
-    // @todo fix 
-    var stylesheetUrl = php_vars.plugin_url+'/css/oxipay-modal.css';
+    var stylesheetUrl = '/wp-content/plugins/oxipay-woocommerce1.3.0/css/oxipay-modal.css';
     var template = '<div class="oxi-modal-overlay"></div>' +
         '<div class="oxi-modal">' +
         '<div class="oxi-modal-content">' +
+        '<div class="oxi-modal-body">' +
+        '<div class="oxi-modal-splash">' +
         '<div class="oxi-modal-header">' +
-        '<img src="'+
-        php_vars.plugin_url+
-        '/images/oxipay.svg" alt="Oxipay"/>' +
-        '<button id="oxi-modal-close" type="button">&times;</button>' +
+        '<div class="oxi-modal-logo"></div>' +
         '</div>' +
-        '<div class="oxi-modal-body"></div>' +
+        '<div class="splash">' +
+        '<div class="splashFrame">' +
+        '<div class="splashGraphic">' +
+        '<svg class="circular" viewBox="25 25 50 50">' +
+        '<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>' +
+        '</svg>' +
+        '<h4 class="splashText">Loading...</h4>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="oxi-modal-iframe"></div>' +
+        '</div>' +
         '</div>' +
         '</div>';
-
-        var model = {
+    var model = {
         targetUrl: '',
         data: data,
         create: create,
@@ -28,9 +37,7 @@ function oxipay(q) {
         setup: setup,
         form: form
     };
-
     return model;
-
     /**
      * Setup Oxipay Checkout
      * @param targetUrl
@@ -38,18 +45,14 @@ function oxipay(q) {
      */
     function setup(targetUrl, keyValue) {
         targetUrl = targetUrl || '/';
-        if (targetUrl.substr(0,4) == "http") {
-            // targetUrl is fine and absolute
-        } else if (targetUrl.substr(0, 1) !== "/") {
+        if (targetUrl.substr(0, 1) !== "/")
             targetUrl = window.location.pathname + targetUrl;
-        }
-
         model.targetUrl = targetUrl;
         var baseUrl = getBaseUrl(targetUrl);
-        
+        stylesheetUrl = baseUrl + stylesheetUrl;
         model.data = keyValue;
-        if (stylesheetUrl) {
-            stylesheetUrl = stylesheetUrl;
+        if (!initialised) {
+            initialised = true;
             setStyle(q(document).find('head'));
         }
     }
@@ -58,8 +61,9 @@ function oxipay(q) {
      */
     function show() {
         try {
-            model.create(); 
-            setTimeout(function () { return model.form.submit(); }, 100);
+            model.create();
+            model.form.submit();
+            setTimeout(function () { return message(null); }, 5000);
         }
         catch (e) {
             console.error(e);
@@ -76,6 +80,15 @@ function oxipay(q) {
             console.error(e);
         }
     }
+    function message(e) {
+        try {
+            $('.oxi-modal-splash').addClass('animated fadeOut');
+            setTimeout(function () { return $('.oxi-modal-splash').remove(); }, 2000);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
     function create() {
         try {
             var body = q('body');
@@ -84,40 +97,29 @@ function oxipay(q) {
             body.append(modal);
             // Insert iframe inside modal body
             var iframe = q("<iframe id=\"" + iframeId + "\"/>");
-            q('.oxi-modal-body').append(iframe);
+            q('.oxi-modal-iframe').append(iframe);
             var form_1 = q(getForm());
             var iframeBody = iframe.contents().find('body');
-            iframeBody.addClass('iframe-body');
-            setStyle(iframe.contents().find('head'));
             iframeBody.append(form_1);
             model.form = form_1;
-            q('#oxi-modal-close').click(model.hide);
+            iFrameResize({ log: true, checkOrigin: false, closedCallback: hide, messageCallback: message }, iframe[0]);
         }
         catch (e) {
             console.error(e);
         }
     }
     function getForm() {
-        var target = window.innerWidth <= 500 ? "_top" : "_self";
-        var loading = '<div class="splash">' +
-            '<div class="splashFrame">' +
-            '<div class="splashGraphic">' +
-            '<svg class="circular" viewBox="25 25 50 50">' +
-            '<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>' +
-            '</svg>' +
-            '</div>' +
-            '</div>' +
-            '</div>';
+        var target = window.innerWidth <= 650 ? "_top" : "_self";
         var form = "<form id=\"oxi-form\" action='" + model.targetUrl + "' method='POST' style=\"display:none;\" target=\"" + target + "\">";
         for (var key in model.data) {
             var val = model.data[key] || "";
             form += "<input type=\"hidden\" name=\"" + key + "\" value=\"" + val + "\"/>";
         }
         form += "</form>";
-        return loading + form;
+        return form;
     }
     function setStyle(head) {
-       head.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + stylesheetUrl + "\">");
+        head.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"" + stylesheetUrl + "\">");
     }
     function getBaseUrl(url) {
         var a = document.createElement('a');
