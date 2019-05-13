8a7c1e2de6170b073f6507d2ccd60ac4bd2294ee
Tao Wang <hellotaotao@gmail.com>
2019-05-13 16:00:47 +0930

    Australia is now totally with humm

--- WC_Flexi_Gateway_Oxipay.php
+++ WC_Flexi_Gateway_Oxipay.php
@@ -216,14 +216,6 @@ abstract class WC_Flexi_Gateway_Oxipay extends WC_Payment_Gateway {
                 'description' => __( 'While test mode is enabled, transactions will be simulated and cards will not be charged', 'woocommerce' ),
                 'desc_tip'    => true
             ),
-            'force_humm'                          => array(
-                'title'       => __( 'Force humm', 'woocommerce' ),
-                'type'        => 'checkbox',
-                'label'       => __( 'Force display and checkout with <strong>humm</strong>, not waiting for automatic switch over (AU only)', 'woocommerce' ),
-                'default'     => 'no',
-                'description' => __( 'You will switch to <strong>humm</strong> if this is set to \'yes\' and the store is in Australia. Otherwise you will be automatically switched over on the official <strong>humm</strong> launch date', 'woocommerce' ),
-                'desc_tip'    => true
-            ),
             'use_modal'                           => array(
                 'title'       => __( 'Modal Checkout', 'woocommerce' ),
                 'type'        => 'checkbox',
--- oxipay-config.php
+++ oxipay-config.php
@@ -9,8 +9,6 @@ class Oxipay_Config {
     const DISPLAY_NAME_AFTER = 'humm';
     const PLUGIN_FILE_NAME = 'oxipay';
     const LAUNCH_TIME_URL = 'https://s3-ap-southeast-2.amazonaws.com/humm-variables/launch-time.txt';
-    const LAUNCH_TIME_DEFAULT = '2019-04-07 14:30:00 UTC';
-    const LAUNCH_TIME_CHECK_ENDS = "2019-10-07 13:30:00 UTC";
     const BUTTON_COLOR = array( "Oxipay" => "E68821", "humm" => "FF6C00" );
 
     public $countries = array(
@@ -33,13 +31,7 @@ class Oxipay_Config {
     );
 
     const URLS = [
-        'AU_Oxipay' => [
-            'sandboxURL'             => 'https://securesandbox.oxipay.com.au/Checkout?platform=WooCommerce',
-            'liveURL'                => 'https://secure.oxipay.com.au/Checkout?platform=WooCommerce',
-            'sandbox_refund_address' => 'https://portalssandbox.oxipay.com.au/api/ExternalRefund/processrefund',
-            'live_refund_address'    => 'https://portals.oxipay.com.au/api/ExternalRefund/processrefund',
-        ],
-        'AU_Humm'   => [
+        'AU'   => [
             'sandboxURL'             => 'https://integration-cart.shophumm.com.au/Checkout?platform=WooCommerce',
             'liveURL'                => 'https://cart.shophumm.com.au/Checkout?platform=WooCommerce',
             'sandbox_refund_address' => 'https://integration-buyerapi.shophumm.com.au/api/ExternalRefund/v1/processrefund',
@@ -66,7 +58,7 @@ class Oxipay_Config {
                 $country = substr( $wc_country, 0, 2 );
             }
         }
-        if ( $country == 'AU' && $this->isAfter() ) {
+        if ( $country == 'AU' ) {
             $name = self::DISPLAY_NAME_AFTER;
         }
 
@@ -75,46 +67,10 @@ class Oxipay_Config {
 
     public function getUrlAddress( $countryCode ) {
         if ( $countryCode == 'AU' ) {
-            return $this->isAfter() ? self::URLS['AU_Humm'] : self::URLS['AU_Oxipay'];
+            return self::URLS['AU'];
         } else {
             return self::URLS['NZ'];
         }
-    }
-
-    private function getLaunchDateString() {
-        $launch_time_string      = get_option( 'oxipay_launch_time_string' );
-        $launch_time_update_time = get_option( 'oxipay_launch_time_updated' );
-        if ( time() - strtotime( self::LAUNCH_TIME_CHECK_ENDS ) > 0 ) {
-            // if after LAUNCH_TIME_CHECK_ENDS time, and launch_time is still empty, set it to default launch time, and done.
-            if ( ! $launch_time_string ) {
-                $launch_time_string = self::LAUNCH_TIME_DEFAULT;
-                update_option( 'oxipay_launch_time_string', $launch_time_string );
-            }
-
-            return $launch_time_string;
-        }
-        if ( empty( $launch_time_string ) || empty( $launch_time_update_time ) || ( time() - $launch_time_update_time >= 1 ) ) {
-            $remote_launch_time_string = wp_remote_get( self::LAUNCH_TIME_URL )['body'];
-            if ( ! empty( $remote_launch_time_string ) ) {
-                $launch_time_string = $remote_launch_time_string;
-                update_option( 'oxipay_launch_time_string', $launch_time_string );
-                update_option( 'oxipay_launch_time_updated', time() );
-            } elseif ( empty( $launch_time_string ) || ( empty( $launch_time_update_time ) && $launch_time_string != self::LAUNCH_TIME_DEFAULT ) ) {
-                // this is when $launch_time_string never set (first time run of the plugin), or local const LAUNCH_TIME_DEFAULT changes and and never update from remote.
-                // Mainly for development, for changing const LAUNCH_TIME_DEFAULT to take effect.
-                $launch_time_string = self::LAUNCH_TIME_DEFAULT;
-                update_option( 'oxipay_launch_time_string', $launch_time_string );
-            }
-        }
-
-        return $launch_time_string;
-    }
-
-    public function isAfter() {
-        $force_humm = get_option( 'woocommerce_oxipay_settings' )['force_humm'];
-
-        return $force_humm == 'yes' ? true : ( time() - strtotime( $this->getLaunchDateString() ) >= 0 );
-
     }
 
     public function getPlatformName() {
