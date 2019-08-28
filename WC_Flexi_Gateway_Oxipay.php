b227a4e4045d78e2fdda9583cdd339c494df1402
Michael <michael.hermann@certegy.com.au>
2019-08-28 09:46:46 +0930

    Added dynamic price-info functionality
    Update widget settings

--- WC_Flexi_Gateway_Oxipay.php
+++ WC_Flexi_Gateway_Oxipay.php
@@ -244,6 +244,21 @@ abstract class WC_Flexi_Gateway_Oxipay extends WC_Payment_Gateway {
                 'default'     => 'yes',
                 'description' => 'Display a price widget in each product page.',
             ),
+            'price_widget_dynamic_enabled'            => array(
+                'title'       => __( 'Dynamic Price Widget', 'woocommerce' ),
+                'type'        => 'checkbox',
+                'label'       => __( 'Enable the humm Dynamic Price Widget' ),
+                'default'     => 'no',
+                'description' => 'Price widget will automatically update breakdown if the product price changes. Uses the CSS selector below to track changes. Leave disabled if unsure.',
+            ),
+            'price_widget_selector' => array(
+                'id'                => $this->pluginFileName . '_api_key',
+                'title'             => __( 'Price Widget CSS Selector', 'woocommerce' ),
+                'type'              => 'text',
+                'default'           => '.price .woocommerce-Price-amount.amount',
+                'description'       => 'CSS selector for the element containing the product price',
+                'desc_tip'          => true,
+            ),
             'preselect_button_enabled'            => array(
                 'title'       => __( 'Pre-select Checkout Button', 'woocommerce' ),
                 'type'        => 'checkbox',
@@ -708,7 +723,6 @@ abstract class WC_Flexi_Gateway_Oxipay extends WC_Payment_Gateway {
                     }
                     $msg = 'complete';
                     break;
-
                 case "failed":
                     $flexi_result_note = __( 'Payment declined using ' . $this->pluginDisplayName . '. Gateway Reference #' . $params['x_gateway_reference'], 'woocommerce' );
                     $order->add_order_note( $flexi_result_note );
@@ -716,7 +730,6 @@ abstract class WC_Flexi_Gateway_Oxipay extends WC_Payment_Gateway {
                     $msg = 'failed';
                     WC()->session->set( 'flexi_result', 'failed' );
                     break;
-
                 case "error":
                     $flexi_result_note = __( 'Payment error using ' . $this->pluginDisplayName . '. Gateway Reference #' . $params['x_gateway_reference'], 'woocommerce' );
                     $order->add_order_note( $flexi_result_note );
