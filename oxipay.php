25b1d6fbaaa195e93dea55bd0cb332db99177ea2
rbi-flexigroup <roger.bi@flexigroup.com.au>
2019-12-19 16:14:03 +1100

    PHPdoc

--- oxipay.php
+++ oxipay.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/* copyright flexigroup
  * Plugin Name: Oxipay Payment Gateway
  * Plugin URI: https://www.oxipay.com.au
  * Description: Easy to setup installment payment plans from <a href="https://oxipay.com.au">Oxipay</a>.
@@ -11,39 +11,56 @@
  * @since 0.4.8
  */
 
-if ( !defined('ABSPATH')) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit;
 
-// this checks that the woocommerce plugin is alive and well.
-include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-if(!is_plugin_active( 'woocommerce/woocommerce.php')) return;
+include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+if (!is_plugin_active('woocommerce/woocommerce.php')) return;
 
-require_once( 'oxipay-config.php' );
+require_once('oxipay-config.php');
 
 add_action('plugins_loaded', 'woocommerce_oxipay_init', 0);
 
 add_action('parse_request', 'get_oxipay_settings');
+
 /**
- * Hook for WC plugin subsystem to initialise the Oxipay plugin
+ *
  */
-function woocommerce_oxipay_init() {
+
+function woocommerce_oxipay_init()
+{
     require_once('WC_Oxipay_Gateway.php');
 }
 
-function add_oxipay_payment_gateway($methods) {
-	$methods[] = 'WC_Oxipay_Gateway';
-	return $methods;
+/**
+ * @param $methods
+ * @return array
+ */
+
+function add_oxipay_payment_gateway($methods)
+{
+    $methods[] = 'WC_Oxipay_Gateway';
+    return $methods;
 }
 
-function add_oxipay_query_vars_filter( $vars ){
+/**
+ * @param $vars
+ * @return array
+ */
+
+function add_oxipay_query_vars_filter($vars)
+{
     $vars[] = "oxi_settings";
     return $vars;
 }
+
 /**
-* Look for an ajax request that wants settings
+ * Look for an ajax request that wants settings
  * @param @query
  * @return null
-*/
-function get_oxipay_settings($query) {
+ */
+
+function get_oxipay_settings($query)
+{
 
     $gateways = WC_Payment_Gateways::instance();
     if (!$gateways) {
@@ -54,7 +71,7 @@ function get_oxipay_settings($query) {
     $list = $gateways->payment_gateways();
     if (!$list || !isset($list['oxipay'])) {
         // abort
-	    return;
+        return;
     }
 
     $oxipay = $list['oxipay'];
@@ -66,11 +83,17 @@ function get_oxipay_settings($query) {
     return;
 }
 
-function oxipay_settings_link($links){
-	$settings_link = array('<a href="'.admin_url('admin.php?page=wc-settings&tab=checkout&section=oxipay').'">Settings</a>');
-	return array_merge($settings_link, $links);
+/**
+ * @param $links
+ * @return array
+ */
+
+function oxipay_settings_link($links)
+{
+    $settings_link = array('<a href="' . admin_url('admin.php?page=wc-settings&tab=checkout&section=oxipay') . '">Settings</a>');
+    return array_merge($settings_link, $links);
 }
 
-add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), 'oxipay_settings_link' );
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'oxipay_settings_link');
 add_filter('woocommerce_payment_gateways', 'add_oxipay_payment_gateway');
-add_filter( 'query_vars', 'add_oxipay_query_vars_filter' );
+add_filter('query_vars', 'add_oxipay_query_vars_filter');
